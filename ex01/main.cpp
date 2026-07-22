ae53efaa301122d92a3ad0d94180905f7ed7a349
Yugo Mizuniwa <ymizuniw@YugonoMacBook-Pro.local>
2026-07-22 18:07:18 +0900

    update: ex01: Makefile written

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -21,7 +21,10 @@
 int main(int argc, char *argv[])
 {
     if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " \"<RPN expression>\"" << std::endl;
         return (1);
+    }
 
     std::string input = argv[1];
     size_t i = 0;
