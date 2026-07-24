b994c38ad9ce5fe7d89faa6519a31d7ae58fc588
Yugo Mizuniwa <ymizuniw@YugonoMacBook-Pro.local>
2026-07-24 20:29:27 +0900

    update: ex02: FordJohnson draft is prceeding. Next is Insertion()

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -24,24 +24,128 @@
 struct IdxValue{
     int idx;
     int value;
+    IdxValue();
+    IdxValue(const IdxValue &other);
+    IdxValue &operator=(const IdxValue &other);
+    bool operator<(const IdxValue &other) const;
+    bool operator<(const IdxValue &other);
 };
 
 struct IdxPair{
-    int large;
-    int small;
+    int large_idx;
+    int small_idx;
+    IdxPair();
+    IdxPair(const IdxValue &other);
+    IdxPair &operator=(const IdxPair &other);
 };
 
+IdxValue::IdxValue()
+{
+    this->idx = -1;
+    this->value = -1;
+}
 
+IdxValue::IdxValue(const IdxValue &other): idx(other.idx), value(other.value)
+{
+    if (this==&other)
+        return ;
+    this->idx = other.idx;
+    this->value = other.value;
+}
 
-std::deque<IdxValue> FordJohnson(std::deque<IdxValue> data)
+IdxValue &IdxValue::operator=(const IdxValue &other)
+{
+    if (this==&other)
+        return ;
+    this->idx = other.idx;
+    this->value = other.value;
+}
+
+bool IdxValue::operator<(const IdxValue &other) const
+{
+    if (this->value<other.value)
+        return (true);
+    return (false);
+}
+
+bool IdxValue::operator<(const IdxValue &other)
+{
+    if (this->value<other.value)
+        return (true);
+    return (false);
+}
+
+void Paring(std::deque<IdxValue> const &data, std::deque<IdxValue> &main_chain, std::deque<IdxValue> &pend_chain, std::deque<IdxPair> &idx_pair)
+{
+    // split data into main_chain and pend_chain keeping the idx of the values
+    size_t size = data.size()/2;
+
+    size_t pair_idx = 0;
+    size_t data_idx = 0;
+    IdxValue large_value = IdxValue();
+    IdxValue small_value = IdxValue();
+    IdxPair idx_pair_tmp = IdxPair();
+
+    while (pair_idx<size)
+    {
+        if (data[data_idx]<data[data_idx+1])
+        {
+            large_value = data[data_idx+1];
+            small_value = data[data_idx];
+            idx_pair_tmp.large_idx = data[data_idx+1].idx;
+            idx_pair_tmp.small_idx = data[data_idx].idx;
+        }
+        else
+        {
+            large_value = data[data_idx];
+            small_value = data[data_idx+1];
+            idx_pair_tmp.large_idx = data[data_idx].idx;
+            idx_pair_tmp.small_idx = data[data_idx+1].idx;
+        }
+        main_chain.push_back(large_value);
+        pend_chain.push_back(small_value);
+        idx_pair.push_back(idx_pair_tmp);
+        ++pair_idx;
+        data_idx += 2;
+    }
+}
+
+void Insertion(std::deque<IdxValue> &sorted_main_chain, std::deque<IdxValue> &pend_chain, std::deque<IdxPair> &idx_pair)
+{
+    // here, pend_chain elements would be inserted into sorted_main_chain, 
+    // but the idx_pair can be lacking for the last element in pend_chain,
+    // and the element cannot be searched by the upper limit of the corresponding element in the sorted_main_chain
+
+    bool odd = (pend_chain.size()%2!=0);
+
+    // from begin() to specific_idx::iterator : paired element
+
+    // from begin() to end() : remaining element
+
+}
+
+std::deque<IdxValue> FordJohnson(std::deque<IdxValue> const &data)
 {
     std::deque<IdxValue> main_chain;
     std::deque<IdxValue> pend_chain;
     std::deque<IdxPair> idx_pair;
 
+    // keep remaining data if odd ocunt
+    IdxValue remain = IdxValue();
+    if (data.size()%2!=0)
+    {
+        remain = data[data.size()-1];
+    }
+
     // Pairing()
+    Paring(data, main_chain, pend_chain, idx_pair);
 
     std::deque<IdxValue> sorted_main_chain = FordJohnson(main_chain);
+
+    if (data.size()%2!=0)
+    {
+        pend_chain.push_back(remain);
+    }
 
     // Insertion()
 
