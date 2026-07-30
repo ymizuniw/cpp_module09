c30f4f25423a5870c9976ee8f3c00618fda3821f
Yugo Mizuniwa <ymizuniw@YugonoMacBook-Pro.local>
2026-07-30 18:04:16 +0900

    update:ex02:original FordJohson class definition removed

--- ex02/FordJohnson.hpp
+++ ex02/FordJohnson.hpp
@@ -36,29 +36,4 @@ class FordJohnson {
 
 # include "FordJohnson.tpp"
 
-// // FordJohnson class receives deque<IdxValue> as an argument, and sort it.
-// class FordJohnson {
-//     private:
-//         std::deque<IdxValue> data_;
-//         std::deque<IdxValue> main_chain_;
-//         std::deque<IdxValue> pend_;
-//         PairIndex idx_pair_;
-
-//         IdxValue getIdxValueOfMainChain(int unique_idx);
-//         IdxValue getIdxValueOfPend(int unique_idx);
-//         int      getPositionOfMainChain(int unique_idx);
-//         void pairing();
-//         void sortPend();
-//         void insertion();
-//         void insertByJacobsthal();
-//         int getSpaceSize(std::deque<IdxValue>::const_iterator &pend_it);
-//     public:
-//         FordJohnson();
-//         FordJohnson(std::deque<IdxValue> const &data);
-//         FordJohnson(const FordJohnson &other);
-//         FordJohnson &operator=(const FordJohnson &other);
-//         ~FordJohnson();
-//         std::deque<IdxValue> sort();
-// };
-
 # endif
